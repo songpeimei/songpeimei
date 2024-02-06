4f888f50bdc8952388e0a8679f1791f68e5a15fa
songpeimei <38445810+songpeimei@users.noreply.github.com>
2024-02-06 09:35:29 +0800

    Update main.py
    若干逻辑更新

--- main.py
+++ main.py
@@ -1,47 +1,61 @@
-### 点球大战 ###
 import random
 import os
-attack = '请输入进攻方向1：东，2：南，3：西，4：北'
-defend = '请输入防守方向1：东，2：南，3：西，4：北'
-error_out = '输入错误，即将退出'
-rule_game = '游戏规则：玩家首先输入进攻方向，电脑随机防守，方向一致则为进攻失败，反之则为进攻成功，玩家输入防守方向，电脑随机进攻，每方各五次机会。按任意键继续'
-init_word = '输入1 开始游戏,输入2 查看规则,输入3退出 \n'
-score_player = 0
-score_computer = 0
-while True:
-    if_begin = input(init_word)
-    if if_begin == '2':
-        os.system('cls')
-        print(rule_game)
-        input()
-        continue
-    elif if_begin == '1':
-        os.system('cls')
-        for times in range(1,12):
-            if times == 11:
-                print('游戏结束 \n：玩家得分 %d,电脑得分 %d \n 按任意键结束' % (score_player,score_computer))
-                input()
-                break
-            if times in(1,3,5,7,9):
-                b = random.randint(1,5)
-                attack_player = int(input('玩家第%s 次进攻, %s \n ' % (((times+1)/2),attack)))
-                print("玩家进攻方向 %s,电脑防守方向 %s"  % (attack_player,b))
-                if b != attack_player:
-                    score_player += 1
-                    print("进攻成功，玩家得分+1\n\n")
-                else:
-                    print('进攻失败，玩家不得分\n\n')
+
+def get_user_input(prompt):
+    while True:
+        user_input = input(prompt)
+        if user_input.isdigit():
+            return int(user_input)
+        else:
+            print("输入错误，请输入数字。")
+
+def penalty_shootout():
+    attack_directions = ['东', '南', '西', '北']
+    rule_game = '游戏规则: 玩家首先输入进攻方向，电脑随机防守，方向一致则为进攻失败，反之则为进攻成功。玩家输入防守方向，电脑随机进攻，每方各五次机会。按任意键继续'
+    
+    score_player = 0
+    score_computer = 0
+
+    os.system('cls')
+    print(rule_game)
+    input('按任意键开始游戏')
+
+    for times in range(1, 11):
+        if times % 2 != 0:  # 玩家进攻
+            b = random.choice(attack_directions)
+            attack_player = get_user_input(f'玩家第{times // 2 + 1}次进攻，1:东, 2:南, 3:西, 4:北\n')
+            print(f'玩家进攻方向 {attack_directions[attack_player - 1]}，电脑防守方向 {b}')
+            if attack_directions[attack_player - 1] != b:
+                score_player += 1
+                print('进攻成功，玩家得分+1\n\n')
             else:
-                a = random.randint(1,5)
-                defend_player = int(input('电脑第%s 次进攻, %s \n ' % (((times) / 2), defend)))
-                print('玩家防守方向%s ,电脑进攻方向 %s' %(defend_player,a))
-                if a != defend_player:
-                    score_computer += 1
-                    print("防守失败，电脑得分+1\n\n")
-                else:
-                    print('防守成功，电脑不得分\n\n')
-    elif if_begin == '3':
-        break
-    else:
-        print(error_out)
-        break
+                print('进攻失败，玩家不得分\n\n')
+        else:  # 电脑进攻
+            a = random.choice(attack_directions)
+            defend_player = get_user_input(f'电脑第{times // 2}次进攻，1:东, 2:南, 3:西, 4:北\n')
+            print(f'玩家防守方向 {attack_directions[defend_player - 1]}，电脑进攻方向 {a}')
+            if attack_directions[defend_player - 1] != a:
+                score_computer += 1
+                print('防守失败，电脑得分+1\n\n')
+            else:
+                print('防守成功，电脑不得分\n\n')
+
+    print(f'游戏结束：玩家得分 {score_player}，电脑得分 {score_computer}\n按任意键结束')
+    input()
+
+if __name__ == "__main__":
+    while True:
+        init_word = '输入1开始游戏，输入2查看规则，输入3退出\n'
+        if_begin = get_user_input(init_word)
+
+        if if_begin == 1:
+            penalty_shootout()
+        elif if_begin == 2:
+            os.system('cls')
+            print(rule_game)
+            input()
+        elif if_begin == 3:
+            break
+        else:
+            print('输入错误，即将退出')
+            break
